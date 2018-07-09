7816fc740f0048165cef04ca10f09f26ae59bb48
Duncan Faulkner <duncan_faulkner@hotmail.com>
2018-07-09 23:16:13 +0100

    create menu/dashboard

---
+++ src/app/app.routing.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutsComponent } from './layouts/layouts.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: LayoutsComponent,
+    data: {
+      title: 'Home'
+    },
+    children: [
+      {
+        path: 'home',
+        loadChildren: './home/home.module#HomeModule'
+      },
+      //   {
+      //     path: 'product',
+      //     loadChildren: './product/product.module#ProductModule'
+      //   },
+      //   {
+      //     path: 'contact',
+      //     loadChildren: './contact/contact.module#ContactModule'
+      //   },
+      {
+        path: 'about',
+        loadChildren: './about/about.module#AboutModule'
+      }
+      //   {
+      //     path: 'shopping-cart',
+      //     loadChildren: './shopping-cart/shopping-cart.module#ShoppingCartModule'
+      //   }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/layouts/layouts.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-layouts',
+  templateUrl: './layouts.component.html',
+  styleUrls: ['./layouts.component.scss']
+})
+export class LayoutsComponent {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
